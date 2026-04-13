d05532d92268165d16d7e7903e743d048ea2f716
hyeongyu-data <hyeongyu.data@gmail.com>
2026-04-13 10:18:08 +0900

    Hotfix: 컬럼명 오기 표기. 예외처리 구조 문제 수정대기

--- dags/05_mysql_etl.py
+++ dags/05_mysql_etl.py
@@ -1,3 +1,4 @@
+
 '''
 - etl 간단하게 적용, 스마트팩토리상 온도 센서에 대한 ETL 처리, mysql 사용
 '''
@@ -49,7 +50,7 @@ def _extract(**kwargs):
     # 로그는 별도의 프로그램에서 지속적으로 발생시켜야 함(시뮬레이션 기준)
     # 현재는 편의상 airflow에 포함시킴 
 
-    # XCOM을 통해서  task_transform에게 전달 (로그의 경로를 전달, 실 데이터 전달 x(지양))
+    # XCOM을 통해서  task_trasform에게 전달 (로그의 경로를 전달, 실 데이터 전달 x(지양))
     logging.info(f'extract 한 로그 데이터 { file_path } ')
     return file_path
 
@@ -81,39 +82,50 @@ def _trasform(**kwargs):
 
     # 5. csv 경로 xcom을 통해서 개시
     return file_path
-    pass
 
 def _load(**kwargs):
     # csv => df => mysql 적제
-    # 1. csv 경로 획득
+    # 1. csv 경로 획득 -> xcom을 통해서 이전 task(게시자)의 id를 이용하여 추출 <- ti 필요
+    ti = kwargs['ti']
+    csv_path = ti.xcom_pull(task_ids='trasform')
 
-    # 2. csv -> df
+    # 2. csv -> df (도입 근거 => 소규모 데이터이므로)
+    df = pd.read_csv(csv_path)
 
-    # 3. mysql 연결 -> MySqlHook 사용
+    # 3. mysql 연결 => MySqlHook 사용
     mysql_hook = MySqlHook(mysql_conn_id='mysql_default')
-    conn       = mysql_hook.get_conn() # 커넥션 획득
-
+    conn       = mysql_hook.get_conn() # 커넥션 획득 -> I/o 영향 있음(예외처리등 필요, with문)
+    # 7. 전체를 try ~ except로 감싸기(I/O)
+    # 실제는 실패 작업인데, 성공으로 오인할수 있다 -> 예외 던지기 필요함
     try:
         # 4. 커서를 획득하여 
-        with conn.cursor() as cursor:
+        with conn.cursor() as cursor:        
             # 4-1. insert 구문 사용
-            # sql = ""
-            # params = []
-            # cursor.executemany( sql, params )
+            sql = '''
+                insert into sensor_readings
+                (sensor_id, timestamp, temperature_c, temperature_f)
+                values (%s, %s, %s, %s)
+            '''            
+            # 여러 데이터를 한번에 넣을때 유용 => executemany() 대응
+            params = [
+                ( data['sensor_id'],     data['timestamp'], 
+                  data['temperature'], data['temperature_f'] )
+                for _, data in df.iterrows() # 데이터가 없을때까지 반복함 -> 데이터가 한세트씩 추출됨
+            ]
+            logging.info(f'입력할 데이터(파라미터) {params}')
+            cursor.executemany( sql, params ) # 한번에 밀어 넣기
             # 4-2. 커밋
-            # conn.commit()
-            pass
+            conn.commit()
+            logging.info('mysql에 적제 완료')
+            pass        
     except Exception as e:
+        logging.info(f'적제 오류 : {e}') # 예외 던지기로 변경 필요(리뷰때 시도)
         pass
     finally:
-        # 5. 연결종료
+        # 5. 연결 종료
         if conn:
             conn.close()
-    # 5. 커밋
-
-    # 6. 연결 종료
-
-    # 7. 전체를 try ~ except로 감싸기(I/O)
+            logging.info(f'mysql 연결 종료 (뒷정리)')
     
     pass
 
@@ -156,7 +168,7 @@ with DAG(
         task_id = "extract",
         python_callable = _extract
     )
-    task_transform   = PythonOperator(
+    task_trasform   = PythonOperator(
         task_id = "trasform",
         python_callable = _trasform
     )
@@ -166,5 +178,5 @@ with DAG(
     )
 
     # 5. 의존성 정의 -> 시나리오별 준비 
-    task_create_table >> task_extract >> task_transform >> task_load
-    #task_extract >> task_transform >> task_load
+    task_create_table >> task_extract >> task_trasform >> task_load
+    #task_extract >> task_trasform >> task_load
