bd929dc0c9f7a1653e28b1e5601085d7f35fb076
hyeongyu-data <hyeongyu.data@gmail.com>
2026-04-14 09:29:14 +0900

    Feat: 기본 구조 및 id 설정 완료

--- dags/07_msa_api_server_used.py
+++ dags/07_msa_api_server_used.py
@@ -12,17 +12,48 @@ import requests # api 호출용, MSA 서비스 호출용
 # 2. API 서버 주소
 API_URL = 'http://127.0.0.1:8000/predict'
 
+# 4-4. 콜백함수 정의
+def _creat_dummy_data(**kwargs):
+    pass
+
+def _api_service_call(**kwargs):
+    pass
+
+def _load_users_credit(**kwargs):
+    pass
+
 # 3. DAG 정의
 with DAG(
-
+    dag_id      = "07_mas_api_server_used", 
+    description = "msa 상에 특정 서비스(ai 서빙 컨셉)를 호출하여 신용평가 수행하는 스케줄링",
+    default_args= {
+        'owner'             : 'de_2team_manager',        
+        'retries'           : 1,
+        'retry_delay'       : timedelta(minutes=1)
+    },
+    schedule_interval = '@daily',
+    start_date  = datetime(2026,2,25),     
+    catchup     = False,
+    tags        = ['msa', 'fastapi'],
 ) as dag:
     # 4. Task 정의
     # 4-1. 더미 데이터 준비 -> 추후 고객 정보 저장 -> 추후 s3 업데이트
-    task_creat_dummy_data = PythonOperator()
+    task_creat_dummy_data = PythonOperator(
+        task_id = "task_creat_dummy_data",
+        python_callable = _creat_dummy_data
+    )
+
     # 4-2. API호출(AI 서비스 활용) -> 신용평가획득
-    task_api_service_call = PythonOperator()
+    task_api_service_call = PythonOperator(
+        task_id = "task_api_service_call",
+        python_callable = _api_service_call
+    )
+
     # 4-3. 결과저장 -> 추후 고객 정보 업데이트
-    task_load_users_credit = PythonOperator()
+    task_load_users_credit = PythonOperator(
+        task_id = "task_load_users_credit",
+        python_callable = _load_users_credit
+    )
 
     # 5. 의존성
     task_creat_dummy_data >> task_api_service_call >> task_load_users_credit
