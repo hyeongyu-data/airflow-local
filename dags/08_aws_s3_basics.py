26ff56e7e6cc62620a1d1b34025948be603f1db8
hyeongyu-data <hyeongyu.data@gmail.com>
2026-04-14 15:22:08 +0900

    Feat: DAG 구성 완료

--- dags/08_aws_s3_basics.py
+++ dags/08_aws_s3_basics.py
@@ -9,6 +9,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 import logging
 from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
@@ -23,6 +24,21 @@ FILE_NAME   = 'hello.txt'
 # 2-3. 업로드할 파일의 로컬내 위치 -> 컨테이너 기반
 LOCAL_PATH = f'/opt/airflow/dags/data/{FILE_NAME}'
 
+def _check_s3(**kwargs):
+    # S3Hook을 통해서 실제 파일이 존재하는지 체크
+    # 1. S3Hook 생성
+    hook = S3Hook(aws_conn_id='aws_default')
+    # 2. 훅을 이용하여 모든 키(객체명, 파일명등등...) 조회
+    keys = hook.list_keys(bucket_name='BUCKET_NAME')
+    # 3. 키 체크
+    if not keys:
+        raise ValueError('업로듯 실패') # 현재 파일만 판단 -> 추후 버킷내 경로를 디테일하게 세분화 필요
+    # 4. 실제로 존재하면 로그 출력
+    for key in keys:
+        logging.info(f'키 : {key}')
+
+    pass
+
 # 3. DAG 정의
 with DAG(
     dag_id      = "08_aws_s3_basics", 
@@ -36,13 +52,25 @@ with DAG(
     start_date  = datetime(2026,2,25),     
     catchup     = False,
     tags        = ['aws', 's3'],
-)
-    # 4. Task 정의
+)as dag:
+    # 4. Task 정의 
+    task_create_file = BashOperator(
+        task_id = "create_file",
+        bash_command = f'echo "hello airflow & s3" > {LOCAL_PATH}'
+    )
     task_upload_to_s3 = LocalFilesystemToS3Operator(
-        task_id = "upload_to_s3"
+        task_id = "upload_to_s3",
+        filename = LOCAL_PATH,  # 로컬 PC등 원본 리소스의 위치(파일명 포함)
+        dest_key = FILE_NAME,   # s3 특정 버킷내에 FILE_NAME으로 저장(생성)
+        dest_bucket = BUCKET_NAME,   # 버킷 네임
+        aws_conn_id = 'aws_default', # aws 접속 정보
+        replace = True      # 동일 파일이 있다면 덮는다
     )
     task_check_s3     = PythonOperator(
-        task_id = "check_s3"
+        task_id = "check_s3",
+        python_callable = _check_s3
     )
+
     # 5. 의존성
-    task_upload_to_s3 >> task_check_s3
+    task_create_file >> task_upload_to_s3 >> task_check_s3
+    pass
