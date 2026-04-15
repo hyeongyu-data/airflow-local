41cfc8dc5e538bf5020f28f41604bce6c2e142fa
hyeongyu-data <hyeongyu.data@gmail.com>
2026-04-15 15:19:51 +0900

    Feat: adf에 데이터 전송 코드 기본

--- log_gen/adf_direct_data_put.py
+++ log_gen/adf_direct_data_put.py
@@ -9,3 +9,22 @@ import json
 import time
 
 # 2. 환경변수
+ACCESS_KEY = ''
+SECRET_KEY = ''
+REGION     = 'ap-northeast-2'
+
+# 3. 특정 서비스(ADF) 클라이언트 생성
+def get_client( service_name='firehose', is_in_aws=True):
+    if not is_in_aws:
+        #    AWS 외부에서 진행
+        session   = boto3.Session(
+            aws_access_key_id       = ACCESS_KEY,
+            aws_secret_access_key   = SECRET_KEY,
+            region_name             = REGION
+        )
+        return session.client(service_name)
+    #  AWS 내부에서 진행
+    return boto3.client(service_name, region_name=REGION)
+
+firehose = get_client()
+print(firehose)
