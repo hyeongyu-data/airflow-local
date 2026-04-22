7e67f86e40d719e7b7c54656b1368acee5b459c8
hyeongyu-data <hyeongyu.data@gmail.com>
2026-04-22 13:11:25 +0900

    Feat: DAG, task 정의

---
+++ dags/11_aws_ma_silver.py
@@ -0,0 +1,60 @@
+'''
+- ma 에서 silver 단계 처리
+- 스케줄 ( 10 * * * * )
+    - firehose에서 버퍼 시간을 최대 3분(180초)로 구성 -> 3분 이후부터는 스케줄 가동 가능함
+    - 보수적으로 10분에 작업하도록 구성
+- 데이터 (flatten, 파생변수, 컬럼명변경) 전처리 수행(sql을 통해)
+    - event_id
+    - event_time => event_timestamp
+    - data.user_id
+    - data.item_id
+    - data.price
+    - data.qty
+    - (data.price * data.qty) as total_price 
+    - data.store_id
+    - source_ip
+    - user_agent
+    - dt (year-month-day)
+    - hour as hr
+- 작업 (silver 테이블 삭제 -> ctas)
+'''
+# 1. 모듈 가져오기
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.athena import AthenaOperator
+
+# 2. 환경변수
+DATABASE_BRONZE = 'de-ai-03-ma-bronze_db'
+DATABASE_SILVER = 'de-ai-03-ma-silver_db'
+SILVER_S3_PATH  = 's3://de-ai-03-827913617635-ap-northeast-2-an/medallion/silver/'
+ATHENA_RESULTS  = 's3://de-ai-03-827913617635-ap-northeast-2-an/athena-results/'
+SILVER_TBL_NAME = 'sales_silver_tbl'
+
+# 3. DAG 정의
+with DAG(
+    dag_id      = "11_medallion_bronze_to_silver_ctas", 
+    description = "athena ctas 작업",
+    default_args= {
+        'owner'             : 'de_2team_manager',        
+        'retries'           : 1,
+        'retry_delay'       : timedelta(minutes=1)
+    },
+    schedule_interval = '10 * * * *', # 매시 10분에 진행
+    start_date  = datetime(2026,2,25),     
+    catchup     = False,
+    tags        = ['aws', 'medallion', 'silver', 'athena', 'ctas'],
+) as dag:
+    # 4. task 정의 (2개)
+    drop_silver_task = AthenaOperator(
+        task_id = 'drop_silver_tbl',
+        query   = 'drop table if exists {{ params.database_silver }}.{{ params.tbl_nm }};',
+        database= DATABASE_SILVER,
+        output_location = ATHENA_RESULTS,
+        params  = {'database_silver':DATABASE_SILVER, 'tbl_nm':SILVER_TBL_NAME} 
+    )
+    ctas_silver_task = AthenaOperator(
+        task_id = 'ctas_silver'
+    )
+
+    # 5. 의존성(injection) 구성
+    drop_silver_task >> ctas_silver_task
