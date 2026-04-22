c9edd2da4342a43b4bafc22cb6ce02b0658c4dfd
hyeongyu-data <hyeongyu.data@gmail.com>
2026-04-22 14:31:34 +0900

    Feat: silver 데이터증분용 DAG

--- dags/11_aws_ma_silver.py
+++ dags/11_aws_ma_silver.py
@@ -26,7 +26,9 @@ from airflow.providers.amazon.aws.operators.athena import AthenaOperator
 # 2. 환경변수
 DATABASE_BRONZE = 'de_ai_03_ma_bronze_db'
 DATABASE_SILVER = 'de_ai_03_ma_silver_db'
+# 데이터 저장용 -> 실데이터
 SILVER_S3_PATH  = 's3://de-ai-03-827913617635-ap-northeast-2-an/medallion/silver/'
+# 쿼리 히스토리등 저장용 -> 메타
 ATHENA_RESULTS  = 's3://de-ai-03-827913617635-ap-northeast-2-an/athena-results/'
 SILVER_TBL_NAME = 'sales_silver_tbl'
 
@@ -59,7 +61,7 @@ with DAG(
         query   = '''
             Create Table if not exists {{ params.database_silver }}.{{ params.tbl_nm }}
             with (
-                format              = 'PARQUER',
+                format              = 'PARQUET',
                 parquet_compression = 'SNAPPY',
                 external_location   = '{{ params.silver_path }}',
                 partitioned_by      = ARRAY['dt','hr']
@@ -81,7 +83,7 @@ with DAG(
             where   year  = '{{ execution_date.format('YYYY') }}'
                 and month = '{{ execution_date.format('MM') }}'
                 and day   = '{{ execution_date.format('DD') }}'
-                and hour  = '{{ execution_date.format('HH') }}'
+                and hour  = '11'
 
         ''',
         database= DATABASE_SILVER,
